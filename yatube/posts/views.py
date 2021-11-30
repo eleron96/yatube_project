ad78ee6cc6cb23719a5dba838d7baa45b39f82fb
Niko Gamsakhurdiya <Eleron96@gmail.com>
2021-12-01 00:44:02 +0300

    Start -Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post
 
 # Create your views here.
 
 
 def index(request):
-    template = 'posts/index.html'
-    title = 'Главная страница'
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
     context = {
-        # В словарь можно передать переменную
-        'title': title,
-        # А можно сразу записать значение в словарь. Но обычно так не делают
-        'text': 'Главная страница',
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def posts_list(request):
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -15,7 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 
